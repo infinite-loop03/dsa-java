7f913551fce4a06b9599855a1702272570adce08
aishwary-03 <aishwary987654321@gmail.com>
2026-06-27 19:51:14 +0530

    Combination Sum

---
+++ src/main/java/com/dsa/patterns/revisit/backtracking/CombinationSum.java
@@ -0,0 +1,35 @@
+package com.dsa.patterns.revisit.backtracking;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CombinationSum {
+
+    public static void main(String[] args) {
+        CombinationSum combinationSum = new CombinationSum();
+        int[] candidates = {2, 3, 5};
+        int target = 8;
+        System.out.println(combinationSum.combinations(candidates, target));
+    }
+
+    private List<List<Integer>> combinations(int[] candidates, int target) {
+        List<List<Integer>> results = new ArrayList<>();
+        backtrack(candidates, target, new ArrayList<>(), results, 0);
+        return results;
+    }
+
+    private void backtrack(int[] candidates, int remaining, List<Integer> path, List<List<Integer>> results, int start) {
+        if (remaining < 0) return;
+
+        if (remaining == 0) {
+            results.add(new ArrayList<>(path));
+            return;
+        }
+
+        for (int i = start; i < candidates.length; i++) {
+            path.add(candidates[i]);
+            backtrack(candidates, remaining - candidates[i], path, results, i);
+            path.removeLast();
+        }
+    }
+}
