fb165d24606ce1046df817a78821fd3c5a9a7145
aishwary-03 <aishwary987654321@gmail.com>
2026-08-15 10:11:55 +0530

    Queue Reconstruction By Height

---
+++ src/main/java/com/dsa/patterns/greedy/QueueReconstructionByHeight.java
@@ -0,0 +1,38 @@
+package com.dsa.patterns.greedy;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class QueueReconstructionByHeight {
+
+    public static void main(String[] args) {
+        QueueReconstructionByHeight queueReconstructionByHeight = new QueueReconstructionByHeight();
+        int[][] people = {
+                {7, 0},
+                {4, 4},
+                {7, 1},
+                {5, 0},
+                {6, 1},
+                {5, 2}
+        };
+        System.out.println(Arrays.deepToString(queueReconstructionByHeight.reconstructQueue(people)));
+    }
+
+    public int[][] reconstructQueue(int[][] people) {
+        Arrays.sort(people, (a, b) -> {
+            if (a[0] != b[0]) {
+                return b[0] - a[0];
+            } else {
+                return a[1] - b[1];
+            }
+        });
+
+        List<int[]> result = new ArrayList<>();
+        for (int[] person : people) {
+            result.add(person[1], person);
+        }
+
+        return result.toArray(new int[result.size()][]);
+    }
+}
