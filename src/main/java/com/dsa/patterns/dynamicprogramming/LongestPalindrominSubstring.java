42424ebaa631809e432a58efff52e0b801e9bd63
aishwary-03 <aishwary987654321@gmail.com>
2026-08-19 12:45:57 +0530

    Longest Palindrome Substring

---
+++ src/main/java/com/dsa/patterns/dynamicprogramming/LongestPalindrominSubstring.java
@@ -0,0 +1,62 @@
+package com.dsa.patterns.dynamicprogramming;
+
+public class LongestPalindrominSubstring {
+
+    public static void main(String[] args) {
+        LongestPalindrominSubstring longestPalindrominSubstring = new LongestPalindrominSubstring();
+        String s = "babad";
+        System.out.println(longestPalindrominSubstring.longestPalindromeUsingDP(s));
+        System.out.println(longestPalindrominSubstring.longestPalindrome(s));
+    }
+
+    public String longestPalindrome(String s) {
+        String maxStr = s.substring(0, 1);
+
+        for (int i = 0; i < s.length(); i++) {
+            String odd = expandFromCenter(s, i, i);
+            String even = expandFromCenter(s, i, i + 1);
+
+            if (odd.length() > maxStr.length()) maxStr = odd;
+            if (even.length() > maxStr.length()) maxStr = even;
+        }
+        return maxStr;
+    }
+
+    private String expandFromCenter(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+        return s.substring(left + 1, right);
+    }
+
+    public String longestPalindromeUsingDP(String s) {
+        int n = s.length();
+        boolean[][] dp = new boolean[n][n];
+        int[] ans = new int[]{0, 0};
+
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = true;
+        }
+
+        for (int i = 0; i < n - 1; i++) {
+            if (s.charAt(i) == s.charAt(i + 1)) {
+                dp[i][i + 1] = true;
+                ans[0] = i;
+                ans[1] = i + 1;
+            }
+        }
+
+        for (int diff = 2; diff < n; diff++) {
+            for (int i = 0; i < n - diff; i++) {
+                int j = i + diff;
+                if (s.charAt(i) == s.charAt(j) && dp[i + 1][j - 1]) {
+                    dp[i][j] = true;
+                    ans[0] = i;
+                    ans[1] = j;
+                }
+            }
+        }
+        return s.substring(ans[0], ans[1] + 1);
+    }
+}
