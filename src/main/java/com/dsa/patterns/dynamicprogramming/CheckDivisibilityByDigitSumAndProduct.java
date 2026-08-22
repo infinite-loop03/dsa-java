2e37e271f2ca7e5207865faadd0289b756df51cf
aishwary-03 <aishwary987654321@gmail.com>
2026-08-22 13:52:31 +0530

    Check Divisibility By Digit Sum and Product

---
+++ src/main/java/com/dsa/patterns/dynamicprogramming/CheckDivisibilityByDigitSumAndProduct.java
@@ -0,0 +1,26 @@
+package com.dsa.patterns.dynamicprogramming;
+
+public class CheckDivisibilityByDigitSumAndProduct {
+
+    public static void main(String[] args) {
+        CheckDivisibilityByDigitSumAndProduct checkDivisibilityByDigitSumAndProduct = new CheckDivisibilityByDigitSumAndProduct();
+        int n = 0;
+        System.out.println(checkDivisibilityByDigitSumAndProduct.checkDivisibility(n));
+    }
+
+    public boolean checkDivisibility(int n) {
+        int sum = 0;
+        int product = 1;
+        int num = n;
+
+        while (n != 0) {
+            int digit = n % 10;
+            sum += digit;
+            product *= digit;
+            n = n / 10;
+        }
+
+        return num % (sum + product) == 0;
+    }
+
+}
