82234a74e0374a750d8bd1d4d6303110a5f51921
aishwary-03 <aishwary987654321@gmail.com>
2026-08-22 17:40:23 +0530

    Coin Change

--- src/main/java/com/dsa/patterns/dynamicprogramming/CoinChange.java
+++ src/main/java/com/dsa/patterns/dynamicprogramming/CoinChange.java
@@ -1,11 +1,27 @@
 package com.dsa.patterns.dynamicprogramming;
 
+import java.util.Arrays;
+
 public class CoinChange {
 
     public static void main(String[] args) {
-
+        CoinChange coinChange = new CoinChange();
+        int[] coins = {1, 2, 5};
+        int amount = 11;
+        System.out.println(coinChange.coinChange(coins, amount));
     }
 
-
-
+    public int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+        for (int a = 1; a <= amount; a++) {
+            for (int coin : coins) {
+                if (a >= coin && dp[a - coin] != Integer.MAX_VALUE) {
+                    dp[a] = Math.min(dp[a], 1 + dp[a - coin]);
+                }
+            }
+        }
+        return dp[amount] == Integer.MAX_VALUE ? -1 : dp[amount];
+    }
 }
