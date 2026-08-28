edb6fffd81b2d9f6e7d23802f961bf02a1508346
aishwary-03 <aishwary987654321@gmail.com>
2026-08-28 21:46:15 +0530

    Longest Common Subsequence

---
+++ src/main/java/com/dsa/patterns/revisit/dynamicprogramming/LongestCommonSubsequence.java
@@ -0,0 +1,26 @@
+package com.dsa.patterns.revisit.dynamicprogramming;
+
+public class LongestCommonSubsequence {
+
+    public static void main(String[] args) {
+        LongestCommonSubsequence longestCommonSubsequence = new LongestCommonSubsequence();
+        String s = "abcde";
+        String t = "ace";
+        System.out.println(longestCommonSubsequence.longestCommonSubsequence(s, t));
+    }
+
+    public int longestCommonSubsequence(String s, String t) {
+        int[][] dp = new int[s.length() + 1][t.length() + 1];
+
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = 1; j <= t.length(); j++) {
+                if (s.charAt(i - 1) == t.charAt(j - 1)) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[s.length()][t.length()];
+    }
+}
