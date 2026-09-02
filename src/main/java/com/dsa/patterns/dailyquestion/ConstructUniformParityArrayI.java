6aab4459409dfebeab889dbca099ee51277ea3c3
aishwary-03 <aishwary987654321@gmail.com>
2026-09-02 09:02:19 +0530

    Construct Uniform Parity Array I

---
+++ src/main/java/com/dsa/patterns/dailyquestion/ConstructUniformParityArrayI.java
@@ -0,0 +1,15 @@
+package com.dsa.patterns.dailyquestion;
+
+public class ConstructUniformParityArrayI {
+
+    public static void main(String[] args) {
+        ConstructUniformParityArrayI constructUniformParityArrayI = new ConstructUniformParityArrayI();
+        int[] nums = {2, 3, 4, 5};
+        System.out.println(constructUniformParityArrayI.uniformArray(nums));
+    }
+
+    public boolean uniformArray(int[] nums1) {
+        return true;
+    }
+
+}
