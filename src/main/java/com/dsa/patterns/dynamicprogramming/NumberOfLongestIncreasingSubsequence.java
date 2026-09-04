e346cc1c00f8b755eb23aaa7c3fc048f33e81dfd
aishwary-03 <aishwary987654321@gmail.com>
2026-09-04 09:59:26 +0530

    Number of Longest Increasing Subsequence

---
+++ src/main/java/com/dsa/patterns/dynamicprogramming/NumberOfLongestIncreasingSubsequence.java
@@ -0,0 +1,47 @@
+package com.dsa.patterns.dynamicprogramming;
+
+import java.util.Arrays;
+
+public class NumberOfLongestIncreasingSubsequence {
+
+    public static void main(String[] args) {
+        NumberOfLongestIncreasingSubsequence numberOfLongestIncreasingSubsequence = new NumberOfLongestIncreasingSubsequence();
+        int[] nums = {2, 2, 2, 2, 2};
+        System.out.println(numberOfLongestIncreasingSubsequence.findNumberOfLIS(nums));
+    }
+
+    public int findNumberOfLIS(int[] nums) {
+        int n = nums.length;
+        int[] length = new int[n];
+        int[] count = new int[n];
+
+        Arrays.fill(length, 1);
+        Arrays.fill(count, 1);
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[j] < nums[i]) {
+                    if (length[j] + 1 > length[i]) {
+                        length[i] = length[j] + 1;
+                        count[i] = count[j];
+                    } else if (length[j] + 1 == length[i]) {
+                        count[i] += count[j];
+                    }
+                }
+            }
+        }
+
+        int maxLength = 0;
+        int answer = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (length[i] > maxLength) {
+                maxLength = length[i];
+                answer = count[i];
+            } else if (length[i] == maxLength) {
+                answer += count[i];
+            }
+        }
+        return answer;
+    }
+}
