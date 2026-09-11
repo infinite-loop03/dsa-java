0ed6b7d757714d972703409dbf4a86ed35cc0716
aishwary-03 <aishwary987654321@gmail.com>
2026-09-11 11:56:40 +0530

    Range Sum Query

--- src/main/java/com/dsa/patterns/revision/arrays/RangeSumQuery.java
+++ src/main/java/com/dsa/patterns/revision/arrays/RangeSumQuery.java
@@ -1,28 +1,37 @@
 package com.dsa.patterns.revision.arrays;
 
+import java.util.Objects;
+
 public class RangeSumQuery {
 
     public static void main(String[] args) {
         int[] nums = {-2, 0, 3, -5, 2, -1};
         NumArray numArray = new NumArray(nums);
-        System.out.println(numArray.sumRange(0, 2));
+        System.out.println(numArray.sumRange(2, 5));
     }
 
     static class NumArray {
 
-        private final int[] nums;
+        private final int[] sumRange;
 
         public NumArray(int[] nums) {
-            this.nums = nums;
+            if (nums == null || nums.length == 0) throw new IllegalArgumentException("Nums should have at least 1 element");
+            sumRange = new int[nums.length];
+            sumRange[0] = nums[0];
+            for (int i = 1; i < nums.length; i++) {
+               sumRange[i] = sumRange[i - 1] + nums[i];
+            }
         }
 
         public int sumRange(int left, int right) {
-            int sum = 0;
-            while (left <= right) {
-                sum += nums[left];
-                left++;
+            if (left > right || left < 0 || right >= sumRange.length) {
+                throw new IllegalArgumentException("Invalid values of left and right");
             }
-            return sum;
+
+            if (left > 0) {
+                return sumRange[right] - sumRange[left - 1];
+            }
+            return sumRange[right];
         }
     }
 
